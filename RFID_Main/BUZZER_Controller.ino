84c8fd52642ac64c5777b7cf950208b86b26782d
JonasVanGool <vangooljonas@gmail.com>
2016-04-05 12:40:31 +0200

    Implemented BUZZER controller

--- RFID_Main/BUZZER_Controller.ino
+++ RFID_Main/BUZZER_Controller.ino
@@ -12,6 +12,11 @@
 
 
 int BUZZER_Sequence[4][10][2];
+boolean m_CmdStart = false;
+byte m_Cmd = -1;
+boolean m_CmdReady = true; 
+byte m_CurrentNote = 0;
+long m_StartTimeNote = 0;
 
 void BUZZER_Setup(){
   BUZZER_Sequence[ADD][0][0] = 440; //Hz
@@ -20,9 +25,33 @@ void BUZZER_Setup(){
   BUZZER_Sequence[ADD][1][1] = 40; //ms
   BUZZER_Sequence[ADD][2][0] = 440; //Hz
   BUZZER_Sequence[ADD][2][1] = 1000; //ms
+  BUZZER_Sequence[ADD][3][0] = -1; //Hz
+
 }
 
-void BUZZER_Loop(){
-  
+void BUZZER_Loop(){ 
+  if(!m_CmdReady){
+    m_CmdStart = false;
+    if(millis()> (BUZZER_PIN,BUZZER_Sequence[m_Cmd][m_CurrentNote][1] + m_StartTimeNote)){ 
+      m_CurrentNote ++;
+      if(BUZZER_PIN,BUZZER_Sequence[m_Cmd][m_CurrentNote][0] == -1){
+         m_CmdReady = true;
+      }else{
+        tone(BUZZER_PIN,BUZZER_Sequence[m_Cmd][m_CurrentNote][0]);
+        m_StartTimeNote = millis();
+      }
+    }  
+  } 
+}
+
+void BUZZER_StartSequence(byte BuzzerSequence){
+  // Initialize variables
+  m_Cmd = BuzzerSequence;
+  m_CmdStart = true;
+  m_CmdReady = false;
+  m_CurrentNote == 0;
+  // Start first note
+  tone(BUZZER_PIN,BUZZER_Sequence[m_Cmd][m_CurrentNote][0]);
+  m_StartTimeNote = millis();
 }
 
--- RFID_Main/EEPROM_Controlller.ino
+++ RFID_Main/EEPROM_Controlller.ino
@@ -3,11 +3,22 @@
  * EEPROM
 ---------------------------------------------------------------
 */
+#include <EEPROM.h>
 
+#define MAX_NR_BADGES 10
+#define BADGE_LENGTH 10
+char m_MasterBadge[BADGE_LENGTH] = {0,0,0,0,0,0,0,0,0,0};
+char m_StoredBadges[MAX_NR_BADGES][BADGE_LENGTH];
+ 
 void EEPROM_Setup(){
-  
+  EEPROM_ReadBadgesFromMemory();  
 }
 
 void EEPROM_Loop(){
   
 }
+
+boolean EEPROM_ReadBadgesFromMemory(){
+  EEPROM.get(0,m_StoredBadges);
+}
+
